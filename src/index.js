0086a4fab89c454e9f6f7fc5bbf308658f2ccad4
aravindmatelabs <aravind@matelabs.in>
2020-04-19 03:14:02 +0530

    local storage done

--- src/index.js
+++ src/index.js
@@ -1,19 +1,38 @@
 import lodash from "lodash";
 
-
 var elemid = 0
+var ls = []
+
+var initPos = []
+var finalPos = []
+
+
 var buttonclick = document.getElementById('addnotes')
 buttonclick.addEventListener('click', openPrompt)
+
+
+if (localStorage.getItem('taskadded') != null) {
+    var getTasks = localStorage.getItem('taskadded')
+    var parseTask = JSON.parse(getTasks)
+
+    parseTask.map(item => {
+        var taskName = item.taskname
+        var parId = item.parentId
+        console.log(taskName)
+        addTodoList(taskName, parId)
+    })
+}
+var parentId = 'todoColumn'
 
 //getting the task in terms of a modal thingy
 function openPrompt() {
     var openModal = prompt('add notes', 'notes')
-    addTodoList(openModal)
+    addTodoList(openModal, parentId)
 }
 
 
 //creating a div eelement in todo column and adding event listeners
-function addTodoList(openModal) {
+function addTodoList(openModal, parentId) {
 
     var inputText = document.createElement('div')
     inputText.style.backgroundColor = "aliceblue"
@@ -21,15 +40,33 @@ function addTodoList(openModal) {
     inputText.innerHTML = openModal
     inputText.draggable = "true"
     inputText.setAttribute('id', 'draggingele' + elemid)
-    document.getElementById('todoColumn').appendChild(inputText)
+
+    document.getElementById(parentId).appendChild(inputText)
     elemid++
+    let initobj = {}
+    initobj['id'] = inputText.id
+    initobj['parentId'] = parentId
+    initobj['taskname'] = openModal
+
+    if (openModal != null) {
+        initPos.push(initobj)
+    }
+    console.log(initPos)
+    storeInls()
+
     inputText.addEventListener('dragstart', (event) => {
         ondragstart(event)
     })
     inputText.addEventListener('dragend', (event) => {
         ondragend(event)
     })
+
 }
+
+function storeInls() {
+    localStorage.setItem('taskadded', JSON.stringify(initPos))
+}
+
 
 //in proress column handling events
 var dropHereIpc = document.getElementById('inProgresscolumn')
@@ -59,17 +96,16 @@ dropInComplete.addEventListener('drop', (event) => {
 function ondragstart(event) {
     var x = event.currentTarget
     x.style.border = "dashed"
-    console.log(event.dataTransfer)
+
 
     var datatransfer = event.dataTransfer.setData('text/plain', event.target.id)
-    var addIntoArr = event.dataTransfer.types.push(datatransfer)
-    console.log(addIntoArr)
+
 }
 
 //on dragend
 function ondragend(event) {
     event.currentTarget.style.border = "none"
-    console.log('heelo')
+
 }
 
 //on dragover
@@ -85,7 +121,22 @@ function ondrop(event) {
     const dropzone = event.currentTarget
     event.currentTarget.style.border = "none"
     dropzone.appendChild(draggablelem)
+
+    initPos.map(item => {
+        if (item.id == draggablelem.id) {
+            if (item.parentId != draggablelem.parentElement.id) {
+                item.parentId = draggablelem.parentElement.id
+            }
+        }
+
+    })
+    storeInls()
+    console.log(initPos)
+
+
 }
+
+
 
 
 
